ed2124cbdb999b8f6976f56c3c4e57660818df01
Preston <preston.truong1@outlook.com>
2023-10-25 11:18:59 -0700

    Update proxy.js

--- static/js/proxy.js
+++ static/js/proxy.js
@@ -1,25 +1,47 @@
-function proxyUrl() {
-    var targetUrl = document.getElementById('targetUrl').value;
+fetch('https://publicsuffix.org/list/public_suffix_list.dat')
+    .then(response => response.text())
+    .then(data => {
+        var tlds = data.split('\n')
+                      .filter(line => !line.startsWith('//') && line.trim() !== '')
+                      .map(line => line.trim());
 
-    if (!/^https?:\/\//i.test(targetUrl)) {
-        targetUrl = 'http://' + targetUrl;
-    }
+        function isValidTld(url) {
+            return tlds.some(tld => url.endsWith('.' + tld));
+        }
 
-    var tldRegex = /\.(com|org|net|gov|edu|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)$/i;
+        function appendDefaultTld(url) {
+            if (!isValidTld(url)) {
+                return url + '.com';
+            }
+            return url;
+        }
 
-    if (!tldRegex.test(targetUrl)) {
-        targetUrl += '.com';
-    }
+        function proxyUrl() {
+            var targetUrl = document.getElementById('targetUrl').value;
+            if (!/^https?:\/\//i.test(targetUrl)) {
+                targetUrl = 'http://' + targetUrl;
+            }
 
-    var proxyContent = document.getElementById('proxyContent');
+            targetUrl = appendDefaultTld(targetUrl);
 
-    fetch(`/proxy?url=${encodeURIComponent(targetUrl)}`)
-        .then(response => response.text())
-        .then(data => {
-            proxyContent.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            proxyContent.innerHTML = 'Error occurred while fetching the content.';
+            var proxyContent = document.getElementById('proxyContent');
+
+            fetch(`/proxy?url=${encodeURIComponent(targetUrl)}`)
+                .then(response => response.text())
+                .then(data => {
+                    proxyContent.innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    proxyContent.innerHTML = 'Error occurred while fetching the content.';
+                });
+        }
+
+        document.getElementById('urlForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            proxyUrl();
         });
-}
+    })
+    .catch(error => {
+        console.error('Error fetching TLD data:', error);
+    });
