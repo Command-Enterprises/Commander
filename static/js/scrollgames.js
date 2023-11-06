ab0164fd20b10cca3bbf4cba29d84b419fac861a
Quartinal <148902345+Quartinal@users.noreply.github.com>
2023-11-06 15:32:54 -0800

    Hope this works :\

--- static/js/scrollgames.js
+++ static/js/scrollgames.js
@@ -1,25 +1,29 @@
 const imageLink = document.querySelector('div.games div.wrapper a');
 const image = imageLink.querySelector('img');
-const imageLinks = ['../games/twoplayergames.html', '../games/btd5.html', '../games/dinogame.html', '../games/geodash.html', '../games/houseofhazards.html', '../games/janissarybattles.html', '../games/janissarytower.html', '../games/minigiants.html', '../games/motox3m.html', '../games/passwordgame.html', '../games/retrobowl.html', '../games/slopegame.html'];
+const imageLinks = ['../games/btd5.html', '../games/dinogame.html', '../games/geodash.html', '../games/houseofhazards.html', '../games/janissarybattles.html', '../games/janissarytower.html', '../games/minigiants.html', '../games/motox3m.html', '../games/passwordgame.html', '../games/retrobowl.html', '../games/slopegame.html'];
 const imageSources = ['../assets/appicons/twoplayergames.svg', '../assets/appicons/btd5.svg', '../assets/appicons/trex.svg', '../assets/appicons/geodash.svg', '../assets/appicons/houseofhazards.svg', '../assets/appicons/janissarybattles.svg', '../assets/appicons/janissarytower.svg', '../assets/appicons/minigiants.svg', '../assets/appicons/motox3m.svg', '../assets/appicons/thepasswordgame.svg', '../assets/appicons/retrobowl.svg', '../assets/appicons/slopegame.svg'];
 const imageClasses = ['twoplayergames', 'btd5', 'trex', 'geodash', 'houseofhazards', 'janissarybattles', 'janissarytower', 'minigiants', 'motox3m', 'thepasswordgame', 'retrobowl', 'slopegame'];
 const imageTitles = ['Two Player Games', 'Bloons TD 5', 'Geometry Dash', 'House Of Hazards', 'Janissary Battles', 'Janissary Tower', 'Minigiants.io', 'Password Game', 'Retro Bowl', 'Slope Game'];
 const imageAlts = ['Two Player Games', 'Bloons TD 5', 'Geometry Dash', 'House Of Hazards', 'Janissary Battles', 'Janissary Tower', 'Minigiants.io', 'Password Game', 'Retro Bowl', 'Slope Game'];
 let currentIndex = 0;
+const gamesPerPage = 3;
 
-function changeAttributes()
+function changeAttributes() 
 {
-    if (currentIndex < 0)
+    if (currentIndex + gamesPerPage < imageSources.length) 
     {
-        currentIndex = imageSources.length - 1;
-    }
-    else if (currentIndex >= imageSources.length)
+        currentIndex += gamesPerPage;
+    } 
+    else 
     {
         currentIndex = 0;
     }
+
     image.style.opacity = 0;
-    image.style.transition = '0.3s ease';
-    setTimeout(() => {
+    image.style.transition = 'opacity 0.5s';
+
+    setTimeout(() => 
+    {
         imageLink.href = imageLinks[currentIndex];
         image.src = imageSources[currentIndex];
         image.title = imageTitles[currentIndex];
@@ -31,13 +35,21 @@ function changeAttributes()
 
 window.addEventListener('wheel', (event) => 
 {
-    if (event.deltaY > 0)
+    if (event.deltaY > 0) 
     {
-        currentIndex++;
-    }
-    else if (event.deltaY < 0)
+        changeAttributes();
+    } 
+    else if (event.deltaY < 0) 
     {
-        currentIndex--;
+        if (currentIndex - gamesPerPage >= 0) 
+        {
+            currentIndex -= gamesPerPage;
+        } 
+        else 
+        {
+            const remaining = imageSources.length % gamesPerPage;
+            currentIndex = imageSources.length - remaining;
+        }
+        changeAttributes();
     }
-    changeAttributes();
 });
