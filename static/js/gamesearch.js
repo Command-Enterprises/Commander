c0de75c12d8dc258e814732c0345b5776775ab4a
qwertychomp <97177478+qwertychomp@users.noreply.github.com>
2023-11-29 19:47:45 -0800

    no case sensitive

--- static/js/gamesearch.js
+++ static/js/gamesearch.js
@@ -2,7 +2,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const game_links = document.querySelectorAll("#content>a");
 const game_links_spans = document.querySelectorAll("#content>a>span");
 const game_searchbar = document.getElementById("search");
-const valid_que = /[a-zA-z0-9]*/gm;
 console.log("e");
 function arrayToString(fgh){
 	let y = "";
@@ -20,11 +19,11 @@ function show(elem){
 	elem.style.visibility = "visible"
 }
 function doit(e){
-	let search = e.target.value
+	let search = e.target.value.toLowerCase()
 	console.log(search)
 	if(search!=""){
 	for(let jk = 0;jk<game_links.length;jk++){
-		if(game_links_spans[jk].innerHTML.indexOf(search)==-1){
+		if(game_links_spans[jk].innerHTML.toLowerCase().indexOf(search)==-1){
 			hide(game_links[jk]);
 		}
 	}
