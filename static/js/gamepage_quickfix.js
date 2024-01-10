f588dc5e072b9ed48c1c32a3b765c362d5429239
qwertychomp <supersequoia11@gmail.com>
2024-01-09 21:00:53 -0800

    put unfinished changes because my laptops gonna die

---
+++ static/js/gamepage_quickfix.js
@@ -0,0 +1,10 @@
+var screenHeight = window.innerHeight
+var screenWidth = window.innerWidth
+var gameCards = document.querySelectorAll(".game-card");
+var sidebar = document.querySelector(".sidenav")
+var navbar = document.querySelector(".header")
+//cosntants below in vh units
+const distanceFromNavsAndBottom = 1
+const gamecardDistances = 1
+func doTheThing(){
+}
